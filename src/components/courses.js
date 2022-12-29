3121683d7c168186ce4bab7bc1cc0097b6a30407
faizaj0 <72667252+faizaj0@users.noreply.github.com>
2022-12-29 18:29:16 +0000

    Fixed code, so it can run on npm start
    Further changes on courses etc

--- src/components/courses.js
+++ src/components/courses.js
@@ -7,38 +7,9 @@ export default function courses(){
     return(
         <div>
             <Navigation />
-            <h1>List of Courses</h1>
-            
+            <h1>List of Courses</h1>   
         </div>
-        <style>
-            .dropdown {
-                position: relative;
-                display: inline-block
-            }
-            .dropdownButton {
-                background-color: #ad815a; 
-                padding: 1em;
-                font-size: 12px;
-                font-weight: bold;
-            }
-            .dropdown:hover .dropdownOptions {
-                display: block;
-            }
-            .dropdownOptions {
-                background-color: #e8e0dd;
-                position: absolute;
-                display: none;
-            }
-            .dropdownOptions a {
-                color: black;
-                padding: 0.8em 1.1em;
-                text-decoration: none;
-                text-align: center;
-                display: block;
-            }
-            .dropdownOptions a:hover {
-                background-color: #fce883;
-            }
-        </style>
+       
+      
     )
 }
