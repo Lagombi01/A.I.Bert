1ce7edb94f234d83b5c5b0ff71b0d1f08c93e9b1
faizaj0 <72667252+faizaj0@users.noreply.github.com>
2023-01-02 18:48:01 +0000

    small changes

--- src/components/courses.js
+++ src/components/courses.js
@@ -1,44 +1,47 @@
+import { courseData } from "./../courseData";
 import Navigation from './navigation';
 
+const wrapperStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(400px, 1fr))",
+    columnGap: "10px",
+    rowGap: "1em"
+};
 
+const cardStyle = {
+    backgroundColor: "dodgerblue",
+    color: "white",
+    padding: "1rem",
+    height: "fit-content"
+};
 
+const descriptionStyle = {
+    display: "-webkit-box",
+    WebkitBoxOrient: "vertical",
+    WebkitLineClamp: 3,
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  };
 
-export default function courses(){
-    return(
-        <div>
-            <Navigation />
-            <h1>List of Courses</h1>
-            
+const courseImage = {width: "100%"};
+
+export default function courses() {
+    return (
+    <div>
+        <Navigation />
+        <h1>List of Courses</h1>
+        <div style={wrapperStyle}>
+            {courseData.map((data, key) => {
+                return (
+                    <div key={key} style = {cardStyle}>
+                        <div><img src={data.image} style={courseImage}/></div>
+                        <div><strong>{data.name}</strong></div>
+                        <div style={descriptionStyle}>{data.description}</div>
+                        <div><a href ={data.link} target="_blank"><button type="button">Learn More</button></a> </div>
+                    </div>
+                );
+            })}
         </div>
-        <style>
-            .dropdown {
-                position: relative;
-                display: inline-block
-            }
-            .dropdownButton {
-                background-color: #ad815a; 
-                padding: 1em;
-                font-size: 12px;
-                font-weight: bold;
-            }
-            .dropdown:hover .dropdownOptions {
-                display: block;
-            }
-            .dropdownOptions {
-                background-color: #e8e0dd;
-                position: absolute;
-                display: none;
-            }
-            .dropdownOptions a {
-                color: black;
-                padding: 0.8em 1.1em;
-                text-decoration: none;
-                text-align: center;
-                display: block;
-            }
-            .dropdownOptions a:hover {
-                background-color: #fce883;
-            }
-        </style>
+    </div>
     )
 }
