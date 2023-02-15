de6a1538a48fbae64453f90c11f6f927ccc9b7f2
faizaj0 <72667252+faizaj0@users.noreply.github.com>
2023-02-15 14:35:00 +0000

    Learning Journey UI

--- src/components/learningJourney.js
+++ src/components/learningJourney.js
@@ -1,11 +1,70 @@
-import Navigation from "./navigation"
-import './components.css';
+import Navigation from "./navigation";
+import "./components.css";
 
-export default function LearningJourney(){
-    return(
-        <div>
-            <Navigation />
-            <h1>Learning Journey Page</h1>
+export default function LearningJourney() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Learning Journey Page</h1>
+
+      <div class="canvas">
+        <div class="tree">
+          <div class="trunk">
+            <div class="branch">
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+            </div>
+            <div class="branch">
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+            </div>
+            <div class="branch">
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+            </div>
+            <div class="branch">
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+            </div>
+            <div class="branch">
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+            </div>
+            <div class="branch">
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+            </div>
+            <div class="branch">
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+              <div class="leaf"></div>
+            </div>
+            <div class="branch">
+              <div class="leaf"></div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
