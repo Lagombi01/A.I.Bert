7b83e86e5dd2e5a3801c938e8c91e3a3550047ed
faizaj0 <72667252+faizaj0@users.noreply.github.com>
2023-02-27 17:17:54 +0000

    added bookmarking functionality

--- src/components/overlayDetails.js
+++ src/components/overlayDetails.js
@@ -1,57 +1,105 @@
-import skillsbuild from "../Images/skillsbuild.png";
-import mindspark from "../Images/mindspark.png";
-import developer from "../Images/developer.png";
+import React, { useState } from "react";
 import cross from "../Images/cross.png";
 import globalVariables from "./globals/globalVariables";
-import { useEffect } from 'react';
-import './components.css';
+import { useEffect } from "react";
+import "./components.css";
 import bookmark from "../Images/smallBookmark.png";
+import bookmarkFilled from "../Images/checkbox.png"
 import book from "../Images/book.png";
 import checkbox from "../Images/checkbox.png";
 
 export default function Details() {
+  
+	const [isBookmarked, setIsBookmarked] = useState(false); // state to track whether the course is bookmarked
+  const [isCompleted, setIsCompleted] = useState(false); // state to track whether the course is completed
 
-    function returnToAll() {
-        globalVariables.currentCourseID = null;
-        document.getElementsByClassName("overlayCourseSpan")[0].style.opacity = 0;
-        document.getElementsByClassName("overlayCourseSpan")[0].style["pointer-events"] = "none";
-        document.body.style["overflow"] = "auto";
-    }
+	function updateBookmark() {
+    const courseID = globalVariables.currentCourseID;
+    
+    fetch("http://localhost:5000/addbookmark", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+			body: JSON.stringify({
+        token: window.localStorage.getItem("token"),
+				courseID,
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setIsBookmarked(!isBookmarked);
+    })
+    .catch(error => console.error(error));
+  }
+ 
 
-    document.addEventListener('keydown', (event) => {
-        var name = event.key;
-        if (name == "Escape" && document.getElementsByClassName("overlayCourseSpan")[0].style.opacity == 1) {
-            returnToAll();
-        }
-    },false);
+	
+  function updateCompletedCourses() {
+    console.log(globalVariables.currentCourseID);
+		setIsCompleted(!isCompleted);
+  }
 
-    return (
-        <div className="overlayCourseSpan">
-            <div id="blackOverlay"></div>
-            <div className="overlayCourseDetails">
-                <div className="detailBlock">
-                    <div id="detailExit" onClick={returnToAll}>
-                        <img src={cross}/>
-                    </div>
-                    <div className="courseImage">
-                        <img alt="Image"></img>
-                    </div>
-                    <div className="courseTitle">
-                        Title
-                    </div>
-                </div>
-                <div className="detailBlock">
-                    <div className="courseDescription">
-                        <p>Description</p>
-                        <p><i></i></p>
-                    </div>
-                    <div className="responseButtons">
-                        <a target="_blank"><div className="responseButton" id="openCourse"><img src={book} alt="Open Course"/></div></a>
-                        <div className="responseButton" id="bookmark"><img src={bookmark} alt="Bookmark"/></div>
-                        <div className="responseButton" id="markComplete"><img src={checkbox} alt="Mark Complete"/></div>
-                    </div>
-                </div>
-            </div>
+  function returnToAll() {
+    globalVariables.currentCourseID = null;
+    document.getElementsByClassName("overlayCourseSpan")[0].style.opacity = 0;
+    document.getElementsByClassName("overlayCourseSpan")[0].style[
+      "pointer-events"
+    ] = "none";
+    document.body.style["overflow"] = "auto";
+  }
+
+  document.addEventListener(
+    "keydown",
+    (event) => {
+      var name = event.key;
+      if (
+        name == "Escape" &&
+        document.getElementsByClassName("overlayCourseSpan")[0].style.opacity ==
+          1
+      ) {
+        returnToAll();
+      }
+    },
+    false
+  );
+
+  return (
+    <div className="overlayCourseSpan">
+      <div id="blackOverlay"></div>
+      <div className="overlayCourseDetails">
+        <div className="detailBlock">
+          <div id="detailExit" onClick={returnToAll}>
+            <img src={cross} />
+          </div>
+          <div className="courseImage">
+            <img alt="Image"></img>
+          </div>
+          <div className="courseTitle">Title</div>
         </div>
+        <div className="detailBlock">
+          <div className="courseDescription">
+            <p>Description</p>
+            <p>
+              <i></i>
+            </p>
+          </div>
+          <div className="responseButtons">
+            <a target="_blank">
+              <div className="responseButton" id="openCourse">
+                <img src={book} alt="Open Course" />
+              </div>
+            </a>
+            <div className="responseButton" id="bookmark">
+              <img src={isBookmarked ? bookmarkFilled : bookmark} onClick={updateBookmark} alt="Bookmark" />
+            </div>
+            <div className="responseButton" id="markComplete">
+              <img src={checkbox} onClick={updateCompletedCourses} alt="Mark Complete" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -2,12 +2,15 @@ import Navigation from "./navigation";
 import "./components.css";
 import User from "../Images/user.png";
 import { useState, useEffect } from "react";
+import { courseData } from "./../courseData.js";
 import { useHistory } from "react-router-dom";
-
 
 export default function Profile() {
   const [userData, setUserData] = useState(null);
+  const [bookmarklist, setBookmarklist] = useState(null);
+
   useEffect(() => {
+    console.log(courseData)
     fetch("http://localhost:5000/userdata", {
       method: "POST",
       headers: {
@@ -23,37 +26,75 @@ export default function Profile() {
       .then((data) => {
         console.log(data, "userData");
         setUserData(data);
+        console.log(userData);
       })
       .catch((error) => console.error(error));
   }, []);
 
   const handleLogout = () => {
     window.localStorage.clear();
-    window.location.href="/login"
-    
+    window.location.href = "/login";
   };
+
+  const getCoursebyId =() =>{
+    
+  }
 
   return (
     <div>
       <Navigation />
       <div className="profile_container">
-        <div className="profile_details">
-        <img src={User} alt=""></img>
-        {userData && <h4>Welcome {userData.data.username} !</h4>}
-        {userData && userData.data.email && (
-          <p>{userData.data.email}</p>
-        )}
-        {userData && userData.data.experiencelvl && (
-          <p>{userData.data.experiencelvl}</p>
-        )}
         
-        {userData && userData.data.experiencelvl === 3 && <p>Expert</p>}
-        {userData && userData.data.experiencelvl === 2 && <p>Intermediate</p>}
-        {userData && userData.data.experiencelvl === 1 && <p>Beginner</p>}
-        <button onClick={handleLogout}>Log out</button>
+        <div className="profile_details">
+          <div className="column1">
+            <img src={User} className="userimage" alt=""></img>
+
+            <button class="form__button" onClick={handleLogout}>
+            Log out
+          </button>
+          </div>
+          <div className="column2">
+            {userData && <h4>Welcome {userData.data.username} !</h4>}
+
+            {userData && userData.data.email && <h5>{userData.data.email}</h5>}
+            {userData && userData.data.experiencelvl && (
+              <h5>{userData.data.experiencelvl}</h5>
+            )}
+          </div>
+        </div>
+
+        <div className="completion">
+          <h4> Progress </h4>
+          <h1>55%</h1>
+        </div>
+
       </div>
-     
-      </div>
+      <div className="course_display">
+          <div className="completed_courses">
+            <h4>Completed Courses</h4>
+          </div>
+
+          <div className="bookmarked_courses">
+            <div>
+              <h4>Bookmarked Courses</h4>
+            </div>
+
+            <div className="course_list">
+              {userData && userData.data.bookmarks && (
+                <p>
+                  {" "}
+                  {userData.data.bookmarks.map((data, index) => {
+                    return (
+                      <p key={index} className="">
+                        {data}
+                      </p>
+                    );
+                  })}{" "}
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
